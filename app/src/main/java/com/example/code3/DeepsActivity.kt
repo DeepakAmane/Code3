be7af3e8f6f5e0daf3c2d26ae1480e4cf93d4385
DeepakAmane <deepaksubamane@gmail.com>
2022-07-19 03:35:20 +0530

    CURRENT FEATURE BRANCH MERGED WITH MAsTER BRANCH ( 2)

--- app/src/main/java/com/example/code3/DeepsActivity.kt
+++ app/src/main/java/com/example/code3/DeepsActivity.kt
@@ -3,12 +3,9 @@ package com.example.code3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DeepsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // m1
-        // m2
-        // m3
+        setContentView(R.layout.activity_deeps)
     }
 }
