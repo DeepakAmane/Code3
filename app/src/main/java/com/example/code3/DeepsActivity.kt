84fe6af56226e0b04cab5ea4b3ba9047e36e5e1d
DeepakAmane <deepaksubamane@gmail.com>
2022-07-19 03:27:26 +0530

    M4

---
+++ app/src/main/java/com/example/code3/DeepsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.code3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeepsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deeps)
+    }
+}
